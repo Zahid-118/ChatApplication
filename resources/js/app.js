4d450af869fa780a039bbf051a2bc1c67a26a8e3
Zahid <zahid@gmail.com>
2024-04-06 23:09:58 +0500

    add

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+
+import { createApp } from "vue";
+import chat from "./components/chat.vue";
+
+const app = createApp({});
+
+app.component("chat", chat);
+app.mount("#app");
